3daf3561108a6dde86fd46fc0057469855381072
Jace Briggs <bri08006@byui.edu>
2016-06-16 22:21:46 -0600

    committing

--- gulpFile.js
+++ gulpFile.js
@@ -1,9 +1,20 @@
 'use strict';
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 
+var concat = require('gulp-concat');
+
+var paths = {
+  sassSource: ['./public/styles/*.scss']
+};
 gulp.task('sass', function() {
-  return gulp.src('public/styles/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./public/newStyles'))
+  return gulp.src(paths.sassSource)
+  .pipe(sass())
+  .pipe(concat('style.css'))
+  .pipe(gulp.dest('./public/styles/'));
 });
+gulp.task('watch', function(){
+  gulp.watch(paths.sassSource, ['sass']);
+})
+gulp.task('default', ["sass", "watch"]);
